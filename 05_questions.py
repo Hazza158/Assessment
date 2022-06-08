603d99e0d9395dc055149dee39768eee7681e4db
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2022-06-08 14:48:09 +1200

    Add files via upload

--- 05_questions.py
+++ 05_questions.py
@@ -1,29 +1,47 @@
-easy_mode = 0
-medium_mode = 0
-hard_mode = 0
+# Version 2 - error message included when calling function
 
-choose_instruction = ("press <1> for easy, <2> for medium, <3> for hard")
+# functions go here
+from multiprocessing import RLock
+easy_mode = []
+medium_mode = []
+hard_mode = []
+question = (easy_mode, medium_mode, hard_mode)
 
-# ask user for # of rounds, <enter> for infinite mode 
+
+def choice_checker(question, valid_list, error):
+
+    valid = False
+    while not valid:
+
+        #ask user for choice (and put choice in lower case)
+        response = input(question).lower()
+
+        # iterates through list and if response is an item
+        # in the list (or the first letter of an item), 
+        # the full name is returned
+
+        for item in valid_list:
+            if response == item [0] or response == item:
+                return item
+        
+        #output error if item nt in list
+        print(error)
+        print()
 
 
-print()
-print("what difficulty do you want to play on?")
-print()
+#main routine goes here
 
-choose = input("{} or 'xxx' to end: ".format(choose_instruction))
 
-print()
+# lists for checking responses
+rps_list = ["rock", "paper", "scissors", "xxx"]
 
-print("you chose {}".format(choose))
+#loop for testing purposes
+user_choice = ""
+while user_choice != "xxx":
 
-print()
+    #ask user for and check its valid
+    user_choice = choice_checker("choose rock / paper / scissors (r/p/s): ", rps_list, "please choose from rock / paper / scissors (or xxx to quit)")
 
-if choose == "1":
-    print("you are now playing on easy difficulty".format(easy_mode))
+    #print out choice for comparison purposes
+    print("You chose {}".format(user_choice))
 
-if choose == "2":
-    print("you are now playing on medium difficulty".format(medium_mode))
-
-elif choose == "3":
-    print("you are now playing on hard difficulty".format(hard_mode))
