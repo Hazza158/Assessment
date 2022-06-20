cd9ee54cf8866ba1e3145c5f412baaf9787dad3b
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2022-06-21 09:56:36 +1200

    Add files via upload

--- 05_easy_question.py
+++ 05_easy_question.py
@@ -5,12 +5,25 @@ import math
 end = False
 questions_asked = 0
 correct_questions = 0
+incorrect_questions= 0 
 
 while end == False:
     if questions_asked <= 10 | questions_asked >= 10:
         choice = random.choice("+-")
-        number_one = random.randint(1,100)
-        number_two = random.randint(1,100)
-        print(number_one, choice, number_two)
+        num_1 = random.randint(1,10)
+        num_2 = random.randint(1,10)
+        print(num_1, choice, num_2)
         answer = int(input("answer the question"))
+    
+    if choice == "+":
+        real_answer = num_1 + num_2 
+        if answer == real_answer:
+            print("correct")
+        correct_questions =+ 1 
 
+    elif choice == "-":
+        real_answer = num_1 - num_2
+
+
+
+   
