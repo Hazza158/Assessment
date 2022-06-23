2246c906863227eb08965592e185cdd0170f352b
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2022-06-23 13:18:56 +1200

    Add files via upload

--- 05_easy_question.py
+++ 05_easy_question.py
@@ -11,22 +11,31 @@ incorrect_questions= 0
 while end == False:
     if questions_asked <= 10 | questions_asked >= 10:
         choice = random.choice("+-")
+        user_choice = "xxx"
         num_1 = random.randint(1,10)
         num_2 = random.randint(1,10)
         print(num_1, choice, num_2)
-        answer = int(input("answer the question"))
+        answer = int(input("answer the question:"))
     
     if choice == "+":
         real_answer = num_1 + num_2 
         if answer == real_answer:
             print("correct")
         correct_questions =+ 1 
+
+        elif
+        incorrect_questions =+ 1
         
     elif choice == "-":
         real_answer = num_1 - num_2
         if answer == real_answer:
             print("correct")
         correct_questions =+ 1
+
+     
+
+
+
 
 
 
